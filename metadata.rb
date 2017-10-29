3da0bbd3588875a0797a5627f28395d9ecbedb42
Erin Kolp <ekolp@kickbackpoints.com>
2017-10-29 17:15:17 -0600

    Add platform support tag in metadata.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,14 @@ maintainer_email 'erinlkolpfoss@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures osx-users'
 long_description 'Installs/Configures osx-users'
-version '0.2.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+version '0.2.1'
+
+chef_version '>= 12.15' if respond_to?(:chef_version)
+
 issues_url 'https://github.com/erinlkolp/osx-users/issues'
 source_url 'https://github.com/erinlkolp/osx-users'
+
+%w{mac_os_x darwin}.each do |os|
+  supports os
+end
+
